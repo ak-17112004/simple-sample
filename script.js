dc6f09c6069a0127a45a4db614d54788056f6b41
Aswath Karthikeyan Raghu <aswathkarthikeyanr17@gmail.com>
2024-12-02 15:56:32 +0530

    files added

---
+++ script.js
@@ -0,0 +1,73 @@
+/*let obj={
+    name:"Aswath",
+    age:18
+}
+console.log(obj)
+let person = new Object();
+person.name = "John";
+person.age = 30;
+person.isMarried = false;
+console.log(person.name);
+
+--------------------------------------------------------------------------------------*/
+let num1=2345
+let num2=6789
+console.log(num1+num2)
+let name=`Aswath`
+console.log(name)
+console.log(typeof num1)
+console.log(typeof name)
+let a=null
+console.log(typeof a)
+let b=true
+console.log(typeof b)
+let c=[1,2,3,4]
+console.log(typeof c)
+console.log(c)
+let d={
+    a:10,
+    b:20,
+    c:30
+}
+console.log(d)
+console.log(typeof d)
+let e=Math.floor(Math.random()*10)
+console.log(e)
+let obj1=d
+obj1.d=40
+console.log(d)
+obj1.e=50
+console.log(obj1)
+console.log(d.e)
+console.log(obj1.a)
+let str=new String("Aswath")
+let z="Aswath" 
+console.log(str)
+console.log(typeof str)
+console.log(typeof z)
+console.log(z.substring(0,4))
+console.log(z.toUpperCase())
+console.log(z.toLowerCase())
+console.log(z.charAt(5))
+console.log(z.endsWith('h'))
+console.log(z.endsWith('a'))
+console.log(z.startsWith('a'))
+console.log(z.startsWith('A'))
+console.log(z)
+console.log(z.includes('a'))
+console.log(z.includes('Aswath'))
+console.log(z.includes('Aswathk'))
+console.log(z)
+console.log(z.indexOf("A"))
+console.log(z.indexOf("s"))
+console.log(z.indexOf("h"))
+console.log(z.indexOf("b"))//if char not there it will return -1
+console.log(z.split("").reverse().join(""))
+let s1="hello guys this is Aswath Karthikeyan Raghu from Parul University"
+console.log(s1.split("").reverse().join(""))
+
+
+
+
+
+
---
+++ script1.js
@@ -0,0 +1,4 @@
+let num=Math.floor(Math.random()*(200-50) + 50)
+// let num2=50
+// console.log(num+num2)
+console.log(num)
